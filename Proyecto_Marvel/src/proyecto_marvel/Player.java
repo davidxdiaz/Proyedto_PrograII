59d8c439c25ec3d934f121e3c056c9a491edc7e6
davidxdiaz <davidxdiaz@unitec.edu>
2016-10-25 08:22:38 -0600

    Clase Player

--- Proyecto_Marvel/src/proyecto_marvel/Player.java
+++ Proyecto_Marvel/src/proyecto_marvel/Player.java
@@ -16,4 +16,29 @@ public class Player {
     private int puntos=0;
     private int contBuenos=0;
     private int contMalos=0;
+    
+    //Variable Global
+    private static Player player[]= new Player[10];
+    private static int numUsuarios = -1;
+    private static int maxUsuarios = 10;
+    
+    //Constructor
+    public Player(String username,String password,int puntos,int contBuenos,int contMalos){
+        this.username = username;
+        this.password = password;
+        this.puntos = puntos;
+        this.contBuenos = contBuenos;
+        this.contMalos = contMalos;
+    }
+    
+    //Funciones
+    public static void add(String user, String pass, int pts,int cB,int cM)
+   {
+      if (pass.equals(""))
+      {
+         pass = "password";
+      }
+      numUsuarios++;
+      player[numUsuarios] = new Player(user, pass, pts,cB,cM);
+   }
 }
